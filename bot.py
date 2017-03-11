ae530c646393c70c1461ed99b1dbe8b2984c4a30
kolaer <overlordin777@gmail.com>
2017-03-11 19:32:36 +0400

    Работа с телеграммом

---
+++ bot.py
@@ -0,0 +1,31 @@
+from telegram.ext import Updater, MessageHandler, Filters
+from calc import *
+
+fo = open("telegram_token", "r")
+
+updater = Updater(fo.readline())
+
+fo.close()
+
+envs = dict()
+
+
+def text_handler(bot, update):
+    chat_id = update.message.chat_id
+    if not chat_id in envs:
+        envs[chat_id] = Environment()
+
+    text = update.message.text
+
+    try:
+        res = str(calculate(text, envs[chat_id]))
+    except RuntimeError as e:
+        res = str(e.args)
+
+    bot.sendMessage(chat_id=chat_id, text=res)
+
+
+updater.dispatcher.add_handler(MessageHandler(Filters.text, text_handler))
+
+updater.start_polling()
+updater.idle()
