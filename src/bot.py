4498401995ba28436eb74532d467612a35bc4a89
kolaer <overlordin777@gmail.com>
2017-05-11 23:24:19 +0400

    Only run bot, if started bot.py

--- src/bot.py
+++ src/bot.py
@@ -9,42 +9,43 @@ from telegram.ext import Updater, MessageHandler, Filters
 
 from src.calc import *
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-fo = open("telegram_token", "r")
+    fo = open("telegram_token", "r")
 
-updater = Updater(fo.readline().strip())
+    updater = Updater(fo.readline().strip())
 
-fo.close()
+    fo.close()
 
-with shelve.open("MySuperCoolDataBase", writeback=True) as envs:
-    def text_handler(bot, update):
-        """Обработчик сообщений"""
-        chat_id = update.message.chat_id
-        s_chat_id = str(chat_id)
+    with shelve.open("MySuperCoolDataBase", writeback=True) as envs:
+        def text_handler(bot, update):
+            """Обработчик сообщений"""
+            chat_id = update.message.chat_id
+            s_chat_id = str(chat_id)
 
-        if s_chat_id not in envs:
-            data = Environment().get_data()
-            envs[s_chat_id] = data
+            if s_chat_id not in envs:
+                data = Environment().get_data()
+                envs[s_chat_id] = data
 
-        text = update.message.text
+            text = update.message.text
 
-        try:
-            data = envs[s_chat_id]
-            env_calc = Environment()
-            env_calc.set_data(data)
+            try:
+                data = envs[s_chat_id]
+                env_calc = Environment()
+                env_calc.set_data(data)
 
-            res = str(calculate(text, env_calc))
+                res = str(calculate(text, env_calc))
 
-            envs[s_chat_id] = env_calc.get_data()
-        except RuntimeError as e:
-            res = str(e.args)
+                envs[s_chat_id] = env_calc.get_data()
+            except RuntimeError as e:
+                res = str(e.args)
 
-        bot.sendMessage(chat_id=chat_id, text=res)
-        envs.sync()
+            bot.sendMessage(chat_id=chat_id, text=res)
+            envs.sync()
 
 
-    updater.dispatcher.add_handler(MessageHandler(Filters.text, text_handler))
+        updater.dispatcher.add_handler(MessageHandler(Filters.text, text_handler))
 
-    updater.start_polling()
-    updater.idle()
+        updater.start_polling()
+        updater.idle()
